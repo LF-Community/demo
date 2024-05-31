f4bb19505c1750259b98f6976e76f6fb9962c85e
tanneberger <revol-xut@protonmail.com>
2024-05-31 18:16:28 +0200

    init

---
+++ target/src-gen/Main/_main_main.c
@@ -0,0 +1,34 @@
+#include "include/api/schedule.h"
+#include "low_level_platform/api/low_level_platform.h"
+#include "include/Main/Main.h"
+#include "_main_main.h"
+// ***** Start of method declarations.
+// ***** End of method declarations.
+#include "include/api/reaction_macros.h"
+void _main_mainreaction_function_0(void* instance_args) {
+    _main_main_main_self_t* self = (_main_main_main_self_t*)instance_args; SUPPRESS_UNUSED_WARNING(self);
+    
+    #line 5 "/home/revol-xut/workspace/lf/demo/./src/Main.lf"
+    std::cout << "Hello World" << std::endl;
+#line 14 "/home/revol-xut/workspace/lf/demo/target/src-gen/Main/_main_main.c"
+}
+#include "include/api/reaction_macros_undef.h"
+_main_main_main_self_t* new__main_main() {
+    _main_main_main_self_t* self = (_main_main_main_self_t*)lf_new_reactor(sizeof(_main_main_main_self_t));
+    self->_lf__reaction_0.number = 0;
+    self->_lf__reaction_0.function = _main_mainreaction_function_0;
+    self->_lf__reaction_0.self = self;
+    self->_lf__reaction_0.deadline_violation_handler = NULL;
+    self->_lf__reaction_0.STP_handler = NULL;
+    self->_lf__reaction_0.name = "?";
+    self->_lf__reaction_0.mode = NULL;
+    #ifdef FEDERATED_DECENTRALIZED
+    self->_lf__startup.intended_tag = (tag_t) { .time = NEVER, .microstep = 0u};
+    #endif // FEDERATED_DECENTRALIZED
+    self->_lf__startup_reactions[0] = &self->_lf__reaction_0;
+    self->_lf__startup.last_tag = NEVER_TAG;
+    self->_lf__startup.reactions = &self->_lf__startup_reactions[0];
+    self->_lf__startup.number_of_reactions = 1;
+    self->_lf__startup.is_timer = false;
+    return self;
+}
---
+++ target/src-gen/Main/build/core/CMakeFiles/lf-low-level-platform-impl.dir/__/low_level_platform/impl/src/lf_unix_syscall_support.c.o.d
@@ -0,0 +1,24 @@
+core/CMakeFiles/lf-low-level-platform-impl.dir/__/low_level_platform/impl/src/lf_unix_syscall_support.c.o: \
+ /home/revol-xut/workspace/lf/demo/target/src-gen/Main/low_level_platform/impl/src/lf_unix_syscall_support.c \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/stdc-predef.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/unistd.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/features.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/features-time64.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/wordsize.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/timesize.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/sys/cdefs.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/long-double.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/gnu/stubs.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/gnu/stubs-64.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/posix_opt.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/environments.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/types.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/typesizes.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/time64.h \
+ /nix/store/14c6s4xzhy14i2b05s00rjns2j93gzz4-gcc-13.2.0/lib/gcc/x86_64-unknown-linux-gnu/13.2.0/include/stddef.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/confname.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/getopt_posix.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/getopt_core.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/unistd.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/unistd-decl.h \
+ /nix/store/fwh4fxd747m0py3ib3s5abamia9nrf90-glibc-2.39-52-dev/include/bits/unistd_ext.h
